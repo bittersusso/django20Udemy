592017bf78ceda50d598f0f78dbb0107a11cd640
Gabriel dos Anjos <cunha.gabriel@gmail.com>
2018-07-05 14:46:05 -0300

    Aula de Funcoes

--- meuProjeto/views.py
+++ meuProjeto/views.py
@@ -1,7 +1,31 @@
 from django.http import HttpResponse
 
+
 def hello(request):
     return HttpResponse('Ola Mundo')
 
+
 def articles(request, year):
     return HttpResponse('O ano enviado foi: ' + str(year))
+
+
+def lerDoBanco(nome):
+    lista_nomes = [
+        {'nome': 'Ana', 'idade': 20},
+        {'nome': 'Pedro', 'idade': 25},
+        {'nome': 'Joaquim', 'idade': 27}
+    ]
+
+    for pessoa in lista_nomes:
+        if pessoa['nome'] == nome:
+            return pessoa
+    else:
+        return {'nome': 'Nao encontrado', 'idade': 0}
+
+
+def fname(request, nome):
+    result = lerDoBanco(nome)
+    if result['idade'] > 0:
+        return HttpResponse('A pessoa foi encontrada, ela tem ' + str(result['idade']) + ' anos.')
+    else:
+        return HttpResponse('Pessoa na encontrada.')
